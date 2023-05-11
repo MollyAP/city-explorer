c69cb424c773de453f7b9e75b74e39792fb1524e
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-05-11 14:20:36 -0500

    re

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Explorer from './Explorer';
+import Explorer from './Zez';
 import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 
