73a8d6f057900acba3186237d563b6b74db96d7d
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-05-11 11:40:48 -0500

    api

--- src/explorer.js
+++ src/explorer.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { Button, Form, Col, Row } from 'react-bootstrap';
+'import './App.css';'
 
 class Explorer extends Component {
   state = {
-    searchquery: '',
-    location: { lat: null, lon: null, display_name: null },
+    searchQuery: '',
+    location: { lat: null, lon: null, displayName: null },
     mapUrl: ''
   };
 
   getLocation = async () => {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchquery}&format=json`;
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
     const res = await axios.get(API);
     const { lat, lon, display_name } = res.data[0];
-    this.setState({ location: { lat, lon, display_name } });
+    this.setState({ location: { lat, lon, displayName: display_name } });
 
     const mapAPI = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${lat},${lon}&zoom=13`;
     this.setState({ mapUrl: mapAPI });
@@ -21,22 +22,23 @@ class Explorer extends Component {
 
   render() {
     return (
-      <div className="mt-5 container">
+      <div className="explorer-container mt-5">
         <Form>
           <Form.Group as={Row} controlId="formSearch">
-            <Form.Label column sm={2}>
+            <Form.Label className="search-label col-sm-2" column sm={2}>
               Search for a city
             </Form.Label>
             <Col sm={8}>
               <Form.Control
                 type="text"
                 placeholder="Enter a city"
-                value={this.state.searchquery}
-                onChange={(e) => this.setState({ searchquery: e.target.value })}
+                value={this.state.searchQuery}
+                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+                className="search-input"
               />
             </Col>
             <Col sm={2}>
-              <Button variant="primary" onClick={this.getLocation}>
+              <Button variant="primary" onClick={this.getLocation} className="explore-button">
                 Explore!
               </Button>
             </Col>
@@ -44,24 +46,24 @@ class Explorer extends Component {
         </Form>
 
         {this.state.location.lat !== null && this.state.location.lon !== null && (
-          <div className="mt-3">
-            <h3>Welcome to lovely {this.state.location.display_name}!</h3>
+          <div className="result-container mt-3">
+            <h3 className="result-title">Welcome to lovely {this.state.location.displayName}!</h3>
             <Row>
               <Col>
-                <h4>Latitude:</h4>
-                <p>{this.state.location.lat}</p>
+                <h4 className="result-subtitle">Latitude:</h4>
+                <p className="result-text">{this.state.location.lat}</p>
               </Col>
               <Col>
-                <h4>Longitude:</h4>
-                <p>{this.state.location.lon}</p>
+                <h4 className="result-subtitle">Longitude:</h4>
+                <p className="result-text">{this.state.location.lon}</p>
               </Col>
             </Row>
           </div>
         )}
 
         {this.state.mapUrl !== '' && (
-          <div className="mt-3">
-            <img src={this.state.mapUrl} alt="Map of city" />
+          <div className="result-container mt-3">
+            <img className="result-map-image" src={this.state.mapUrl} alt="Map of city" />
           </div>
         )}
       </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Explorer from './explorer';
+import Explorer from './Explorer';
+import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <Header />
     <Explorer />
   </React.StrictMode>
 );
