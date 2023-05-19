18bf9da7dd77991bf45a3ad960032797c68a3a99
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-05-19 10:29:53 -0500

    movies pls

--- src/Explorer.js
+++ src/Explorer.js
@@ -10,24 +10,29 @@ class Explorer extends Component {
     searchQuery: '',
     location: { lat: null, lon: null, displayName: null },
     mapUrl: '',
-    forecast: []
+    forecast: [],
+    movies: [],
+    error: null
   };
 
   getLocation = async () => {
-    try{
-       const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
-    const res = await axios.get(API);
-    const { lat, lon, display_name } = res.data[0];
-    
-    const weatherAPI = `https://city-explorer-api-2ts6.onrender.com/weather?lat=${lat}&lon=${lon}&searchQuery=${this.state.searchQuery}`;
-    const weatherRes = await axios.get(weatherAPI);
-    // Handle the response from the /weather endpoint
-    this.setState({ location: { lat, lon, displayName: display_name }, mapUrl: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${lat},${lon}`, forecast:weatherRes.data });
-    
+    try {
+      const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+      const res = await axios.get(API);
+      const { lat, lon, display_name } = res.data[0];
+
+      const weatherAPI = `https://city-explorer-api-2ts6.onrender.com/weather?lat=${lat}&lon=${lon}&searchQuery=${this.state.searchQuery}`;
+      const weatherRes = await axios.get(weatherAPI);
+      // Handle the response from the /weather endpoint
+      this.setState({ location: { lat, lon, displayName: display_name }, mapUrl: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${lat},${lon}`, forecast: weatherRes.data });
+
+      const movieAPI = `https://city-explorer-api-2ts6.onrender.com/movies?searchQuery=${this.state.searchQuery}`;
+      const movieRes = await axios.get(movieAPI);
+
     }
-  catch(error){
-    this.setState({error})
-  }
+    catch (error) {
+      this.setState({ error })
+    }
   };
 
   render() {
@@ -76,22 +81,27 @@ class Explorer extends Component {
             <img className="result-map-image" src={this.state.mapUrl} alt="Map of city" />
           </div>
         )}
-        
+
         {this.state.forecast.length !== 0 && (
-        <Weather forecastData={this.state.forecast}/>
+          <Weather forecastData={this.state.forecast} />
         )}
 
+        {this.state.movies.length !== 0 && (
+          <Movie movieData={this.state.movies} />
+        )}
+
+
         {this.state.error && (
-        <div className="alert alert-danger" role="alert">
-          <strong>Error {this.state.error.status}: </strong>  
-          <div class="outer-container">
-          <div class="inner-container">
-            <p>Sowwy, the page you are looking for does not exist ówò</p>
+          <div className="alert alert-danger" role="alert">
+            <strong>Error {this.state.error.status}: </strong>
+            <div class="outer-container">
+              <div class="inner-container">
+                <p>Sowwy, the page you are looking for does not exist ówò</p>
+              </div>
+            </div> {this.state.error.message}
           </div>
-        </div> {this.state.error.message}
-        </div>
-        
-      )}
+
+        )}
       </div>
     );
   }
