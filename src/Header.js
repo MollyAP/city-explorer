4f39a3a16f1ca0a1b10cffc0a0591942fd9b2112
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-05-22 18:42:02 -0500

    fix maybe

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h2 className="daHead">
+      <h2 className="daHead" style={{ position: 'fixed', top: 0, width: '100%', zIndex: 999, textAlign: 'center' }}>
         CITY EXPLORER
       </h2>
     );
